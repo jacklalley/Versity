07ed88f84ac64c45b25362c2b704b29be2e61ef0
jacklalley <jckly13@gmail.com>
2013-12-08 00:22:48 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VersityApp::Application.config.secret_token = '25e7a46e85904e0b79c90a3569d923fa4bff9285dba089c4ac5cf9abd2a3d214984c55f5bbc6d5a5f02f26479e17845a74d1ebb0395b1852468a313155c096a7'
